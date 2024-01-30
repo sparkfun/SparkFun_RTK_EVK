b0bb46cc4c0b30e32e1f019e240efcf3c9025899
Paul <pc235603@gmail.com>
2024-01-30 13:03:06 +0000

    Add u-blox Cellular Library examples

---
+++ Example_Sketches/9_4_GNSS_NTRIP_Caster/9_4_GNSS_NTRIP_Caster.ino
@@ -0,0 +1,390 @@
+/*
+  SparkFun RTK EVK Test Sketch
+
+  License: MIT. Please see LICENSE.md for more details
+
+  This example demonstrates how to connect to an NTRIP Caster like Skylark or PointOneNav.
+  Corrections are received by the LARA-R6 and pushed to the GNSS.
+
+  Enter your NTRIP credentials in secrets.h
+
+  ESP32-WROVER-IE Pin Allocations:
+  D0  : Boot + Boot Button
+  D1  : Serial TX (CH340 RX)
+  D2  : STAT LED
+  D3  : Serial RX (CH340 TX)
+  D4  : SD CS
+  D5  : LARA_ON - via 74HC4066 switch and PWREN. Needs PULLDOWN
+  D12 : SDA2 - Qwiic OLED - via 74HC4066 switch and PWREN
+  D13 : Serial1 TX - LARA_TXDI
+  D14 : Serial1 RX - LARA RXDO
+  D15 : SCL2 - Qwiic OLED - via 74HC4066 switch and PWREN
+  D16 : N/A
+  D17 : N/A
+  D18 : SPI SCK
+  D19 : SPI POCI
+  D21 : I2C SDA
+  D22 : I2C SCL
+  D23 : SPI PICO
+  D25 : GNSS Time Pulse
+  D26 : LARA Power On
+  D27 : Ethernet Chip Select
+  D32 : PWREN
+  D33 : Ethernet Interrupt
+  A34 : LARA Network Indicator
+  A35 : Board Detect (3.0V)
+  A36 : SD Card Detect
+*/
+
+const int STAT_LED = 2;
+const int SD_CS = 4; // Chip select for the microSD card
+const int LARA_ON = 5; // High indicates the LARA VCCIO is on. Needs PULLDOWN
+const int SDA_2 = 12; // OLED
+const int SERIAL_TX = 13; // LARA_TXDI
+const int SERIAL_RX = 14; // LARA RXDO
+const int SCL_2 = 15; // OLED
+const int SCL_1 = 22; // ZED-F9P and NEO-D9S
+const int SDA_1 = 21; // ZED-F9P and NEO-D9S
+const int GNSS_INT = 25; // ZED_F9P interrupt
+const int LARA_PWR = 26; // LARA_PWR_ON - inverted - set LARA_PWR high to pull LARA_PWR_ON low
+const int ETHERNET_CS = 27; // Chip select for the WizNet W5500
+const int PWREN = 32; // 74HC4066 switch Enable - pull high to enable SCL2/SDA2 and LARA_ON
+const int ETHERNET_INT = 33; // WizNet W5500 interrupt
+const int LARA_NI = 34; // LARA Network Indicator - only valid when the LARA is powered on
+const int SD_PRESENT = 36; // microSD card card present - from the microSD socket switch
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#include "base64.h" //Built-in ESP32 library
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#include <HardwareSerial.h>
+HardwareSerial laraSerial(2); //UART2 normally uses pins 16 and 17, but these are not available on WROVER
+
+#include <SparkFun_u-blox_Cellular_Arduino_Library.h> //Click here to get the library: http://librarymanager/All#SparkFun_u-blox_Cellular
+
+// Derive the LARA_R6 class, so we can override beginSerial
+class LARA_R6_Derived : public SparkFun_ublox_LARA_R6001D
+{
+  public:
+    LARA_R6_Derived() : SparkFun_ublox_LARA_R6001D{LARA_PWR}{} // Pass the LARA_PWR pin into the class so the library can powerOn / powerOff
+
+  protected:
+    void beginSerial(unsigned long baud) override
+    {
+      delay(100);
+      if (_hardSerial != nullptr)
+      {
+        _hardSerial->end();
+        _hardSerial->begin(baud, SERIAL_8N1, SERIAL_RX, SERIAL_TX); // Configure Serial1
+      }
+      delay(100);
+    }
+};
+
+// Create a LARA_R6 object to use throughout the sketch
+LARA_R6_Derived myLARA;
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#include <SparkFun_u-blox_GNSS_v3.h> //http://librarymanager/All#SparkFun_u-blox_GNSS_v3
+SFE_UBLOX_GNSS myGNSS;
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#include <Wire.h>
+TwoWire I2C_1 = TwoWire(0);
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Globals
+
+volatile int socketNum = -1; // The TCP socket number. -1 indicates invalid/closed socket
+volatile bool connectionOpen = false; // Flag to indicate if the connection to the NTRIP Caster is open
+volatile unsigned long lastReceivedRTCM_ms; // Record when data last arrived - so we can time out if required
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Headers
+
+bool keyPressed(char *c = nullptr);
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+void setup()
+{
+  pinMode(SD_CS, OUTPUT);
+  digitalWrite(SD_CS, HIGH);
+  pinMode(ETHERNET_CS, OUTPUT);
+  digitalWrite(ETHERNET_CS, HIGH);
+
+  // Configure the LARA_PWR GPIO before enabling the regulators
+  pinMode(LARA_PWR, OUTPUT);
+  digitalWrite(LARA_PWR, LOW);
+
+  // Now enable the 3.3V regulators for the GNSS and LARA
+  pinMode(PWREN, OUTPUT);
+  digitalWrite(PWREN, HIGH);
+  
+  pinMode(LARA_NI, INPUT);
+  pinMode(LARA_ON, INPUT_PULLDOWN);
+  
+  delay(1000); // Wait for the ESP32
+
+  Serial.begin(115200);
+  Serial.println("SparkFun RTK EVK - Test Sketch");
+
+  // The LARA_R6 will be powered off by default.
+  // If desired, we can power it on manually by toggling the LARA_PWR pin now.
+  // Or we can wait and let myLARA.begin do it.
+  // else
+  // {
+  //   digitalWrite(LARA_PWR, HIGH);
+  //   delay(100);
+  //   digitalWrite(LARA_PWR, LOW);
+    
+  //   delay(8000); // Wait > 7 seconds for the LARA to begin
+  // }
+
+  //myLARA.enableDebugging();
+
+  myLARA.invertPowerPin(true); //LARA_PWR is inverted
+
+  Serial.println(F("Initializing the LARA_R6. This could take ~20 seconds."));
+
+  // Initialize the LARA
+  if (myLARA.begin(laraSerial, 115200) )
+  {
+    Serial.println(F("LARA-R6 connected!"));
+  }
+  else
+  {
+    Serial.println(F("Unable to communicate with the LARA."));
+  }
+
+  if (!digitalRead(LARA_ON))
+    Serial.println(F("LARA-R6 failed to power on!"));
+
+  Serial.print(F("Waiting for NI to go high"));
+  int tries = 0;
+  const int maxTries = 60;
+  while ((tries < maxTries) && (digitalRead(LARA_NI) == LOW))
+  {
+    delay(1000);
+    Serial.print(F("."));
+    tries++;
+  }
+  Serial.println();
+  if (tries == maxTries)
+    Serial.println(F("NI didn't go high. Giving up..."));
+
+  String currentOperator = "";
+
+  while (Serial.available()) Serial.read();
+  
+  // First check to see if we're connected to an operator:
+  if (myLARA.getOperator(&currentOperator) == UBX_CELL_SUCCESS)
+  {
+    Serial.print(F("Connected to: "));
+    Serial.println(currentOperator);
+  }
+  else
+  {
+    Serial.print(F("The LARA is not yet connected to an operator. Please use the previous examples to connect. Or wait and retry. Freezing..."));
+    while (1)
+      ; // Do nothing more
+  }
+
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+  // Set a callback to process the socket data
+  // This will push the RTCM data to the GNSS
+  myLARA.setSocketReadCallbackPlus(&processSocketData);
+  
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+  // Set a callback to process the socket close
+  myLARA.setSocketCloseCallback(&processSocketClose);
+  
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+  // Start I2C. Connect to the GNSS.
+
+  I2C_1.begin((int)SDA_1, (int)SCL_1); //Start I2C
+
+  // Uncomment the next line to enable the 'major' GNSS debug messages on Serial so you can see what AssistNow data is being sent
+  //myGNSS.enableDebugging(Serial, true);
+
+  if (myGNSS.begin(I2C_1) == false) //Connect to the Ublox module using Wire port
+  {
+    Serial.println(F("u-blox GPS not detected at default I2C address. Please check wiring. Freezing."));
+    while (1);
+  }
+  Serial.println(F("u-blox module connected"));
+
+  myGNSS.setI2COutput(COM_TYPE_UBX | COM_TYPE_NMEA);                 //Set the I2C port to output both NMEA and UBX messages
+  myGNSS.setI2CInput(COM_TYPE_UBX | COM_TYPE_NMEA | COM_TYPE_RTCM3); //Be sure RTCM3 input is enabled. UBX + RTCM3 is not a valid state.
+
+  myGNSS.setDGNSSConfiguration(SFE_UBLOX_DGNSS_MODE_FIXED); // Set the differential mode - ambiguities are fixed whenever possible
+
+  myGNSS.setNavigationFrequency(1); //Set output in Hz.
+
+  // Set the Main Talker ID to "GP". The NMEA GGA messages will be GPGGA instead of GNGGA
+  myGNSS.setMainTalkerID(SFE_UBLOX_MAIN_TALKER_ID_GP);
+
+  myGNSS.newCfgValset(VAL_LAYER_RAM_BBR); // Use cfgValset to disable / enable individual NMEA messages
+  myGNSS.addCfgValset(UBLOX_CFG_MSGOUT_NMEA_ID_GLL_I2C, 0); // Several of these are on by default so let's disable them
+  myGNSS.addCfgValset(UBLOX_CFG_MSGOUT_NMEA_ID_GSA_I2C, 0);
+  myGNSS.addCfgValset(UBLOX_CFG_MSGOUT_NMEA_ID_GSV_I2C, 0);
+  myGNSS.addCfgValset(UBLOX_CFG_MSGOUT_NMEA_ID_RMC_I2C, 0);
+  myGNSS.addCfgValset(UBLOX_CFG_MSGOUT_NMEA_ID_VTG_I2C, 0);
+  myGNSS.addCfgValset(UBLOX_CFG_MSGOUT_NMEA_ID_GGA_I2C, 10); // Output GGA once every 10 seconds
+  if (myGNSS.sendCfgValset()) // Send the configuration VALSET
+    Serial.println(F("NMEA messages were configured successfully"));
+  else
+    Serial.println(F("NMEA message configuration failed!"));
+
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+  // Wait for a 2D fix
+
+  Serial.println(F("Waiting for a 2D fix"));
+
+  byte fixType = 0;
+  while (fixType < 2)
+  {
+    if (myGNSS.getPVT())
+    {
+      long latitude = myGNSS.getLatitude();
+      Serial.print(F("Lat: "));
+      Serial.print(latitude);
+  
+      long longitude = myGNSS.getLongitude();
+      Serial.print(F(" Long: "));
+      Serial.print(longitude);
+  
+      long altitude = myGNSS.getAltitude();
+      Serial.print(F(" Alt: "));
+      Serial.print(altitude);
+  
+      fixType = myGNSS.getFixType();
+      Serial.print(F(" Fix: "));
+      if(fixType == 0) Serial.print(F("No fix"));
+      else if(fixType == 1) Serial.print(F("Dead reckoning"));
+      else if(fixType == 2) Serial.print(F("2D"));
+      else if(fixType == 3) Serial.print(F("3D"));
+      else if(fixType == 4) Serial.print(F("GNSS + Dead reckoning"));
+      else if(fixType == 5) Serial.print(F("Time only"));
+
+      Serial.println();
+    }
+  }
+
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+  // Setup callbacks
+
+  myGNSS.setNMEAGPGGAcallbackPtr(&pushGPGGA); // Set up the callback for GPGGA
+
+  myGNSS.setAutoPVTcallbackPtr(&printPVTdata); // Enable automatic NAV PVT messages with callback to printPVTdata so we can watch the carrier solution go to fixed
+
+  //myGNSS.saveConfiguration(VAL_CFG_SUBSEC_IOPORT | VAL_CFG_SUBSEC_MSGCONF); //Optional: Save the ioPort and message settings to NVM
+
+}
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+void loop()
+{
+  myGNSS.checkUblox(); // Check for the arrival of new GNSS data and process it.
+  myGNSS.checkCallbacks(); // Check if any GNSS callbacks are waiting to be processed.
+
+  myLARA.bufferedPoll(); // Process the URC's. This pushes the incoming RTCM data to the GNSS.
+
+  enum states // Use a 'state machine' to open and close the connection
+  {
+    open_connection,
+    check_connection_and_wait_for_keypress,
+    close_connection,
+    waiting_for_keypress,
+    shutdown_lara,
+    do_nothing
+  };
+  static states state = open_connection;
+
+  //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+  switch (state)
+  {
+    case open_connection:
+      Serial.println(F("Connecting to the NTRIP caster..."));
+      if (beginClient((int *)&socketNum, (bool *)&connectionOpen)) // Try to open the connection to the caster
+      {
+        Serial.println(F("Connected to the NTRIP caster! Press any key to disconnect..."));
+        state = check_connection_and_wait_for_keypress; // Move on
+      }
+      else
+      {
+        Serial.print(F("Could not connect to the caster. Trying again in 5 seconds."));
+        for (int i = 0; i < 5; i++)
+        {
+          delay(1000);
+          Serial.print(F("."));
+        }
+        Serial.println();
+      }
+      break;
+
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    case check_connection_and_wait_for_keypress:
+      // If the connection has dropped or timed out, or if the user has pressed a key
+      if ((checkConnection((int)socketNum, (bool)connectionOpen) == false) || (keyPressed()))
+      {
+        state = close_connection; // Move on
+      }
+      break;
+    
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    case close_connection:
+      Serial.println(F("Closing the connection to the NTRIP caster..."));
+      closeConnection((int *)&socketNum, (bool *)&connectionOpen);
+      Serial.println(F("Press 's' to shut down the LARA-R6"));
+      Serial.println(F("Press any other key to reconnect..."));
+      state = waiting_for_keypress; // Move on
+      break;
+    
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    case waiting_for_keypress:
+      // Wait for the user to press a key
+      char c;
+      if (keyPressed(&c))
+      {
+        if (c != 's')
+          state = open_connection; // Move on
+        else
+          state = shutdown_lara;
+      }
+      break; 
+
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    case shutdown_lara:
+      // Shut down the LARA
+      myLARA.modulePowerOff();
+      Serial.println(F("The LARA has shut down. Reset the RTK to start again."));
+      state = do_nothing;
+      break; 
+
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    case do_nothing:
+    default:
+      break; 
+  }
+}
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
